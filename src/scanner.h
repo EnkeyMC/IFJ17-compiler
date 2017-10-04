9c2ecb3e29bf79eb0d8553e39b484e0e7aacdc01
EnkeyMC <enkeymc@gmail.com>
2017-10-04 15:01:29 +0200

    Add token ID definitions

--- src/scanner.h
+++ src/scanner.h
@@ -12,6 +12,44 @@
 
 #include <stdio.h>
 
+// Token ID definitions
+
+// ARITMETIC OPERATORS
+#define TOKEN_DIVR          10
+#define TOKEN_DIVI          11
+#define TOKEN_MUL           12
+#define TOKEN_ADD           13
+#define TOKEN_SUB           14
+#define TOKEN_EQUAL         15
+
+// BOOLEAN OPERATORS
+#define TOKEN_LT            20
+#define TOKEN_GT            21
+#define TOKEN_LE            22
+#define TOKEN_GE            23
+#define TOKEN_NE            24
+
+// SPECIAL TOKENS
+#define TOKEN_COMMA         30
+#define TOKEN_SEMICOLON     31
+#define TOKEN_LPAR          32
+#define TOKEN_RPAR          33
+
+// STRING TOKENS
+#define TOKEN_IDENTIFIER    40
+#define TOKEN_KEYWORD       41
+#define TOKEN_STRING        42
+#define TOKEN_INT           43
+#define TOKEN_REAL          44
+
+// EXPANSIONS
+#define TOKEN_DIVR_ASIGN    50
+#define TOKEN_DIVI_ASIGN    51
+#define TOKEN_MUL_ASIGN     52
+#define TOKEN_ADD_ASIGN     53
+#define TOKEN_SUB_ASIGN     54
+
+
 typedef struct {
     int id;
     void* attr;
