0186554d1f8b52756ecccaafd9303bfb1385c530
EnkeyMC <enkeymc@gmail.com>
2017-11-08 16:09:06 +0100

    List implementation

--- src/list.c
+++ src/list.c
@@ -1,53 +1,149 @@
+#include <malloc.h>
+#include <assert.h>
 #include "list.h"
 
-List* list_init() {
+List* list_init(free_data_f free_data) {
+	List* l = (List*) malloc(sizeof(List));
+	if (l == NULL) {
+		return NULL;
+	}
 
+	l->first = NULL;
+	l->active = NULL;
+	l->free_data = free_data;
+
+	return l;
 }
 
-void list_free(List* list, void (*free_data_f) (void* data)) {
+void list_free(List* l) {
+	assert(l != NULL);
 
+	list_activate(l);
+	while (list_active(l)) {
+		l->free_data(list_delete_active(l));
+	}
+
+	free(l);
 }
 
 bool list_insert_first(List* l, void* data) {
+	assert(l != NULL);
 
-}
+	ListItem* new_item = (ListItem*) malloc(sizeof(ListItem));
+	if (new_item == NULL) {
+		return false;
+	}
 
-bool list_insert_last(List* l, void* data) {
+	new_item->data = data;
+	new_item->next = l->first;
 
+	l->first = new_item;
+	return true;
 }
 
 bool list_post_insert(List* l, void* data) {
+	assert(l != NULL);
 
+	if (l->active == NULL)
+		return false;
+
+	ListItem* new_item = (ListItem*) malloc(sizeof(ListItem));
+	if (new_item == NULL) {
+		return false;
+	}
+
+	new_item->data = data;
+	new_item->next = l->active->next;
+
+	l->active->next = new_item;
+
+	return true;
 }
 
 bool list_empty(List* l) {
+	assert(l != NULL);
 
+	return l->first == NULL;
 }
 
 bool list_active(List* l) {
+	assert(l != NULL);
 
+	return l->active != NULL;
 }
 
 void* list_get_active(List* l) {
+	assert(l != NULL);
 
+	if (l->active == NULL)
+		return NULL;
+
+	return l->active->data;
 }
 
 void* list_get_first(List* l) {
+	assert(l != NULL);
 
+	if (l->first == NULL)
+		return NULL;
+
+	return l->active->data;
 }
 
-void* list_get_last(List* l) {
+void* list_delete_frist(List* l) {
+	assert(l != NULL);
 
+	if (l->first == NULL)
+		return NULL;
+
+	void* data = l->first->data;
+	ListItem* to_delete = l->first;
+	l->first = l->first->next;
+
+	free(to_delete);
+	return data;
+}
+
+void* list_delete_active(List* l) {
+	assert(l != NULL);
+
+	if (l->active == NULL)
+		return NULL;
+
+	void* data = l->active->data;
+	ListItem* to_delete = l->active;
+
+	if (l->first == l->active) {
+		l->first = l->active->next;
+	}
+
+	l->active = l->active->next;
+
+	free(to_delete);
+	return data;
 }
 
 void list_activate(List* l) {
+	assert(l != NULL);
 
+	l->active = l->first;
 }
 
 void list_succ(List* l) {
-	
+	assert(l != NULL);
+
+	l->active = l->active->next;
 }
 
 int list_length(List* l) {
+	assert(l != NULL);
 
+	int length = 0;
+	list_activate(l);
+	while (list_active(l)) {
+		length++;
+		list_succ(l);
+	}
+
+	return length;
 }
--- src/list.h
+++ src/list.h
@@ -6,32 +6,35 @@
 /**
  * List item
  */
-struct list_item_t {
+typedef struct list_item_t {
     void* data;  /// Generic item data
     struct list_item_t* next;  /// Next item in list
-};
+} ListItem;
+
+typedef void (*free_data_f) (void* data);
 
 /**
  * List object structure
  */
 typedef struct list_t {
-    struct list_item_t* first;  /// First list item
-    struct list_item_t* last;  /// Last list item
-    struct list_item_t* active;  /// Active item
+    ListItem* first;  /// First list item
+    ListItem* active;  /// Active item
+    free_data_f free_data;  /// Function for data deallocation, can be NULL
 } List;
 
 /**
- * Initialize empty list
+ * Initialize list
+ * @param free_data funtion for data dealloction, can be NULL
  * @return new list, NULL on allocation error
  */
-List* list_init();
+List* list_init(free_data_f free_data);
 
 /**
  * Free list and free list data with given function free_data_f
  * @param list List
  * @param free_data_f callback function to free list data, can be NULL
  */
-void list_free(List* list, void (*free_data_f) (void* data));
+void list_free(List* l);
 
 /**
  * Insert item on the first position in the list
@@ -40,14 +43,6 @@ void list_free(List* list, void (*free_data_f) (void* data));
  * @return true on success, false otherwise
  */
 bool list_insert_first(List* l, void* data);
-
-/**
- * Insert item on the last position in the list
- * @param l List
- * @param data generic data to insert
- * @return true on success, false otherwise
- */
-bool list_insert_last(List* l, void* data);
 
 /**
  * Insert item after active item, no effect if list is not active
@@ -86,11 +81,18 @@ void* list_get_active(List* l);
 void* list_get_first(List* l);
 
 /**
- * Get data from last item
+ * Delete first item from list and return it
  * @param l List
- * @return generic data, NULL if list is empty
+ * @return item data
  */
-void* list_get_last(List* l);
+void* list_delete_frist(List* l);
+
+/**
+ * Delete active item from list and return it, activity will shift to next item
+ * @param l List
+ * @return item data
+ */
+void* list_delete_active(List* l);
 
 /**
  * Set first item as active
