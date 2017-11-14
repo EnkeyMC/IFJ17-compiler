11e51af725962abbd15a2d902b32ed98c1efeca1
EnkeyMC <enkeymc@gmail.com>
2017-11-14 17:06:11 +0100

    Refactored ugly code

--- src/expr_parser.c
+++ src/expr_parser.c
@@ -18,10 +18,9 @@ int parse_expression(Parser *parser) {
 	int ret_code = EXIT_SUCCESS;
 	Token* token = NULL;
 
-	while (1) {
+	do {
 		if (token != NULL) {
 			token_free(token);
-			parser->scanner->backlog_token = NULL;
 		}
 
 		token = scanner_get_token(parser->scanner);
@@ -33,9 +32,6 @@ int parse_expression(Parser *parser) {
 			ret_code = EXIT_LEX_ERROR;
 			break;
 		}
-
-		// save current token to shard buffer
-		parser->scanner->backlog_token = token;
 
 		// look for next action in the precedence table
 		unsigned action = get_action(s, token->id);
@@ -49,13 +45,9 @@ int parse_expression(Parser *parser) {
 			}
 		}
 
-		// syntax error encountered by previous loop
-		if (ret_code == EXIT_SYNTAX_ERROR)
-			break;
-		else if (action == EXPR_PUSH_MARKER) {
+		if (action == EXPR_PUSH_MARKER) {
 			if (! ext_stack_push(s, token->id)) {
 				ret_code = EXIT_INTERN_ERROR;
-				break;
 			}
 		}
 		else if (action == EXPR_HANDLE_MARKER) {
@@ -63,18 +55,17 @@ int parse_expression(Parser *parser) {
 				token->id = TOKEN_IDENTIFIER;
 			if (! ext_stack_shift(s, token->id)) {
 				ret_code = EXIT_INTERN_ERROR;
-				break;
 			}
 		}
 		else if (action == EXPR_ERROR) {
 			ret_code = EXIT_SYNTAX_ERROR;
-			break;
 		}
 		else
 			break;
-	}
+	} while (ret_code == EXIT_SUCCESS);
 
-	// Last token is still in the shared buffer
+	// Return token that is not part of expression
+	scanner_unget_token(parser->scanner, token);
 
 	ext_stack_free(s);
 
