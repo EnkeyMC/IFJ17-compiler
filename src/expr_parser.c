60cb66a45bc3c5c919884979f39fd5d3a01342f8
EnkeyMC <enkeymc@gmail.com>
2017-11-17 20:46:42 +0100

    Changed get_action macro to uppercase

--- src/expr_parser.c
+++ src/expr_parser.c
@@ -6,7 +6,7 @@
 #include "ext_stack.h"
 #include "error_code.h"
 
-#define get_action(s, token) (expr_grammar.precedence_table[pt_map_token(ext_stack_top(s))][pt_map_token(token)])
+#define GET_ACTION(s, token) (expr_grammar.precedence_table[pt_map_token(ext_stack_top(s))][pt_map_token(token)])
 
 int parse_expression(Parser *parser) {
 	assert(parser != NULL);
@@ -32,10 +32,10 @@ int parse_expression(Parser *parser) {
 		}
 
 		// look for next action in the precedence table
-		unsigned action = get_action(s, token->id);
+		unsigned action = GET_ACTION(s, token->id);
 		while (action == EXPR_REDUCE_MARKER) {
 			if (ext_stack_reduce(s))
-				action = get_action(s, token->id);
+				action = GET_ACTION(s, token->id);
 			else {
 				// haven't found any rule to reduce the stack
 				ret_code = EXIT_SYNTAX_ERROR;
