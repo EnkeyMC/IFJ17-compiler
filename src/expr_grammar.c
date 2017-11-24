b94040cba543fcf0e391a5e99b13bb5e5a0368dd
Martin Omacht <enkeymc@gmail.com>
2017-11-24 15:35:32 +0100

    Added header comment to all files

--- src/expr_grammar.c
+++ src/expr_grammar.c
@@ -1,3 +1,12 @@
+/**
+ * File is part of project IFJ2017.
+ *
+ * Brno University of Technology, Faculty of Information Technology
+ *
+ * @package IFJ2017
+ * @authors xomach00 - Martin Omacht, xchova19 - Zdeněk Chovanec, xhendr03 - Petr Hendrych
+ */
+
 #include <stdarg.h>
 #include <malloc.h>
 #include <assert.h>
